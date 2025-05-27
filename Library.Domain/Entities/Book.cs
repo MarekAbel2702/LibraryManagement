69a8e9a611a8ee368216501280a12d53b7e1ee80
MarekAbel2702 <marek-abel@wp.pl>
2025-05-27 16:38:01 +0200

    Dodanie encji w Domain

---
+++ Library.Domain/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; } 
+        public int AuthorId { get; set; }
+        public int GenreId {  get; set; }
+
+        public Author Author { get; set; }
+        public Genre Genre {  get; set; }
+    }
+}
