3a74e6c0ac13be7741e97deba24d11eeb6d4873b
MarekAbel2702 <marek-abel@wp.pl>
2025-05-28 13:53:18 +0200

    Dodanie dwukierunkowego mapowania

---
+++ Library.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using Library.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Library.Application.DTOs;
+
+namespace Library.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Book, BookDto>().ReverseMap();
+            CreateMap<Author, AuthorDto>().ReverseMap();
+            CreateMap<Genre, GenreDto>().ReverseMap();
+        }
+    }
+}
