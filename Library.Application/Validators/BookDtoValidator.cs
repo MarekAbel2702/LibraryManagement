8ebd81449dbaaaf9dbd2f88ca5ac8401ac3f5cfe
MarekAbel2702 <marek-abel@wp.pl>
2025-05-28 13:58:52 +0200

    Dodanie walidacji dla : AuthorDto, BookDto, GenreDto

---
+++ Library.Application/Validators/BookDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Library.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Application.Validators
+{
+    public class BookDtoValidator : AbstractValidator<BookDto>
+    {
+        public BookDtoValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().Length(2, 100);
+            RuleFor(x => x.AuthorId).GreaterThan(0);
+            RuleFor(x => x.GenreId).GreaterThan(0);
+        }
+    }
+}
---
+++ Library.Application/Validators/GenreDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Library.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Application.Validators
+{
+    public class GenreDtoValidator : AbstractValidator<GenreDto>
+    {
+        public GenreDtoValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().Length(2, 100);
+        }
+    }
+}
